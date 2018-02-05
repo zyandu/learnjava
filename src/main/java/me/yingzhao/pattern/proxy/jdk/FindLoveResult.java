2f1f50ad8fef9708defa314136a07db64c230c97
zyandu <zyandu@gmail.com>
2018-02-05 23:36:24 +0800

    设计模式：jdk动态代理

---
+++ src/main/java/me/yingzhao/pattern/proxy/jdk/FindLoveResult.java
@@ -0,0 +1,9 @@
+package me.yingzhao.pattern.proxy.jdk;
+
+public class FindLoveResult {
+    public static void main(String[] args) throws Throwable {
+        Person op = (Person) new MeiPo().getInstance(new MeiMei());
+        op.findLove();
+
+    }
+}
---
+++ src/main/java/me/yingzhao/pattern/proxy/jdk/Person.java
@@ -0,0 +1,7 @@
+package me.yingzhao.pattern.proxy.jdk;
+
+public interface Person {
+    public void findLove();
+    String getSex();
+    String getName();
+}
